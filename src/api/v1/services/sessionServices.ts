c6d34acd4a110df113476e248cf39142a1f5c18b
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-04-03 10:19:44 +0200

    feat: Session creation via reMarkable device token.
    I connected the Session service with the API context. The context now extracts the authentication token from the request headers and pass it down to the service.
    
    This allows the service to request a session token in behalf of a device.

--- src/api/v1/services/sessionServices.ts
+++ src/api/v1/services/sessionServices.ts
@@ -2,8 +2,8 @@ import { Device } from 'a-remarkable-js-sdk'
 import { type ResolversTypes } from '../graphql'
 import { serializeSession } from '../serializers/sessionSerializers'
 
-export async function createSession (): Promise<ResolversTypes['Session']> {
-  const device = new Device('token goes here')
+export async function createSession (deviceToken: string): Promise<ResolversTypes['Session']> {
+  const device = new Device(deviceToken)
   await device.connect()
   const session = device.session
   return serializeSession(session)
